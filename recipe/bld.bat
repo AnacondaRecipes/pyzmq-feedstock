c0f97ecfab68359ca9cb95087ef6724ccccfffc8
Michael Sarahan <msarahan@gmail.com>
2017-09-20 14:07:20 -0500

    build->host for cross

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,5 +2,5 @@ set DISTUTILS_USE_SDK=1
 
 set ZMQ=%LIBRARY_PREFIX%
 
-"%PYTHON%" -m pip install . "--install-option=--zmq=%ZMQ%"
+python -m pip install . "--install-option=--zmq=%ZMQ%"
 if errorlevel 1 exit 1
