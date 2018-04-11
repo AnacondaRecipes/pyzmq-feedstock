f48a4d9b258716ba0da9aa12b327d427d4cac826
Ray Donnelly <mingw.android@gmail.com>
2018-04-11 22:06:32 +0100

    Use zeromq on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,11 @@
 set DISTUTILS_USE_SDK=1
 
 set ZMQ=%LIBRARY_PREFIX%
+<<<<<<< HEAD
 
+=======
+:: set ZMQ=bundled
+copy %LIBRARY_PREFIX%\bin\libzmq.lib %LIBRARY_PREFIX%\lib\libzmq.lib
+>>>>>>> Use zeromq on Windows
 python -m pip install . "--install-option=--zmq=%ZMQ%"
 if errorlevel 1 exit 1
