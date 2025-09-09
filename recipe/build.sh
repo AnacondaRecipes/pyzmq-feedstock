9fd5d9f800a1c2dc42e9492514a899830b0b7b43
Patryk Kups <pkups@anaconda.com>
2025-09-09 12:24:03 +0200

    PKG-9583: pyzmq 27.0.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 set -x
 
 # make sure it doesn't fallback on bundled libzmq
 export PYZMQ_NO_BUNDLE=1
 export SKBUILD_CMAKE_VERBOSE=true
 
-"${PYTHON}" -m pip install --no-deps --no-build-isolation .
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation --ignore-installed
