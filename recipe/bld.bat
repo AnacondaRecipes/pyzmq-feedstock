9149b9de980f9262b02e25c5b3871c55aed8eb5f
Patryk Kups <pkups@anaconda.com>
2025-09-15 13:45:56 +0200

    check

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,5 @@
 set PYZMQ_NO_BUNDLE=1
 set SKBUILD_CMAKE_VERBOSE=true
-set CMAKE_GENERATOR=Ninja
 
 %PYTHON% -m pip install --no-deps --no-build-isolation --config-settings="cmake.build-type=Release" -vv .
 if errorlevel 1 exit 1
