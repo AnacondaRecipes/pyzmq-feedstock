9e14f1ccb779028f38204354f1dd04cfb885b964
Patryk Kups <pkups@anaconda.com>
2025-09-16 10:45:21 +0200

    check

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,6 @@ set -x
 # make sure it doesn't fallback on bundled libzmq
 export PYZMQ_NO_BUNDLE=1
 export SKBUILD_CMAKE_VERBOSE=true
+export CMAKE_BUILD_TYPE=Release
 
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation --ignore-installed
