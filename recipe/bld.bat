94aa3a814c5c1af58fcbf3acdc208fb451d541b8
PatrykKups <pkups@anaconda.com>
2025-09-17 12:09:50 +0200

    pyzmq 27.0.2  (#17)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 set PYZMQ_NO_BUNDLE=1
 set SKBUILD_CMAKE_VERBOSE=true
-set CMAKE_GENERATOR=Ninja
+set CMAKE_BUILD_TYPE=Release
 
-"%PYTHON%" -m pip install --no-deps --no-build-isolation --config-settings="cmake.build-type=Release" -vv .
+%PYTHON% -m pip install --no-deps --no-build-isolation -vv .
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 set -x
 
 # make sure it doesn't fallback on bundled libzmq
 export PYZMQ_NO_BUNDLE=1
 export SKBUILD_CMAKE_VERBOSE=true
+export CMAKE_BUILD_TYPE=Release
 
-"${PYTHON}" -m pip install --no-deps --no-build-isolation .
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation --ignore-installed
