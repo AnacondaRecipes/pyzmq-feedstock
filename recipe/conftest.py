ffc624cd7913fd97f5a020f2a50f5ac0f19adf02
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-09-04 12:46:47 +0200

    skip tests on Windows

--- recipe/conftest.py
+++
@@ -1,9 +0,0 @@
-import sys
-import asyncio
-import pytest
-
-@pytest.fixture(scope="session")
-def event_loop_policy():
-    if sys.platform == "win32":
-        return asyncio.WindowsSelectorEventLoopPolicy()
-    return asyncio.get_event_loop_policy()
